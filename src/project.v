12d5642562101393a0f7e23cebf8fc1bee800d9a
SRB2149 <141785894+SRB2149@users.noreply.github.com>
2026-09-06 00:53:11 +0100

    Added um to name
    Added um to name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_sr_ga1_srb2149 (
+module tt_um_sr_ga1_srb2149 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
