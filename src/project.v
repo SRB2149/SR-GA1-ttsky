ed98caeb292affd35faaa97bf07f2c1653b75dc4
SRB2149 <141785894+SRB2149@users.noreply.github.com>
2026-09-06 01:05:52 +0100

    Update project.v
    Changed wire to logic

--- src/project.v
+++ src/project.v
@@ -6,18 +6,18 @@
 `default_nettype none
 
 module tt_um_sr_ga1_srb2149 (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  logic [7:0] ui_in,    // Dedicated inputs
+    output logic [7:0] uo_out,   // Dedicated outputs
+    input  logic [7:0] uio_in,   // IOs: Input path
+    output logic [7:0] uio_out,  // IOs: Output path
+    output logic [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  logic       ena,      // always 1 when the design is powered, so you can ignore it
+    input  logic       clk,      // clock
+    input  logic       rst_n     // reset_n - low to reset
 );
 
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, 1'b0};
+    logic _unused = &{uio_in[5:3], ena, 1'b0};
     
     logic [1:0] ddio_dir;
 
